57310d60e52bcb928d6db8663f7b4c01e082cbeb
vanessaborges2 <vanessa.borges2@fatec.sp.gov.br>
2024-03-22 19:50:17 -0300

    Primeiro Exercício

--- Primeiroprojetocomposer/bootstrap.php
+++ Primeiroprojetocomposer/bootstrap.php
@@ -5,6 +5,7 @@ require __DIR__."/vendor/autoload.php";
 $metodo = $_SERVER['REQUEST_METHOD'];
 $caminho = $_SERVER['PATH_INFO'] ?? '/';
 
+#use Php\Primeiroprojeto\Router
 $r = new Php\Primeiroprojeto\Router($metodo, $caminho);
 
 #ROTAS
@@ -13,11 +14,20 @@ $r->get('/olamundo', function (){
     return "Olá mundo!";
 } );
 
-#ROTAS
-
 $r->get('/olapessoa/{nome}', function($params){ 
     return 'Olá'.$params[1]; 
 } );
+
+$r->get('/exer1/formulario', function(){
+    include("exer1.html");
+});
+
+$r->post('/exer1/resposta', function(){
+    $valor1 = $_POST['valor1'];
+    $valor2 = $_POST['valor2'];
+    $soma = $valor1 + $valor2;
+    return "A soma é: {$soma}";
+});
 
 #ROTAS
 
